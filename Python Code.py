6bdf11c1e6b3b2cb754391cda4176bc012b40dfa
YunZng <71421800+YunZng@users.noreply.github.com>
2020-09-25 16:23:04 -0400

    Update Python Code.py

--- Python Code.py
+++ Python Code.py
@@ -1,30 +1,23 @@
+# Create all essential lists/dictionary
 inputTextList=[]
 outputTextList=[]
-compressionChars=[]
-compressionShortBinary=[]
-compressionLongBinary=[]
-counter = 0
+cChars=[]
+cShortBinary=[]
+cLongBinary=[]
+dList = {}
 
-
-my_file = open(input("Enter file name: (example.txt):  "))
-inputTextList = my_file.read()
-#inputType=int(input("What are you trying to do?\n(1)Text to binary\n(2)Binary to text\n"))
-
+# Add all the long binary to the list
 for i in range (64):
-	compressionLongBinary.append("{0:{fill}7b}".format(i,fill="0"))
-temp = 0
-Dlist = {}
+	cLongBinary.append("{0:{fill}7b}".format(i,fill="0"))
+	
+# Add all the lower case letters to the list
 for x in range(26):
-   # print(compressionLongBinary[x])
-    print(chr(x+97))
-    Dlist[chr(x+97)] = compressionLongBinary[x] 
-    print(Dlist[chr(x+97)]) # to test out if the keys and values are actually in the dictionary verbosely 
+    dList[chr(x+97)] = cLongBinary[x]
 
+# Add all the upper case letter to the list
 for y in range(27, 53, 1):
-    print(chr(y+38))
-    Dlist[chr(y+38)] = compressionLongBinary[y-1]
-    print(Dlist[chr(y+38)]) # to test out if the keys and values are actually in the dictionary verbosely 
+    dList[chr(y+38)] = cLongBinary[y-1]
 
-
-#Mods to the ASCII ordered dictionary 
-#Dlist[character] = bit value 
+# Print all the items in matching order
+for a, b in dList.items():
+    print (a,b)
