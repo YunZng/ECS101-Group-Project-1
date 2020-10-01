4168bda317f7c9e8aa27e4968e7e87670c186917
YunZng <71421800+YunZng@users.noreply.github.com>
2020-10-01 13:19:37 -0400

    Update Python Code.py

--- Python Code.py
+++ Python Code.py
@@ -16,36 +16,38 @@ def getkey(want):
 	for key, value in dList.items():
 		if value == want:
 			return key
-	return "No such thing"
 
 # Add all the long binary to the list
 for i in range (64):
 	cLongBinary.append("{0:{fill}7b}".format(i,fill="0"))
 
+# Add the short bits 
+for i in range (16):
+	cShortBinary.append("1"+"{0:{fill}4b}".format(i,fill="0"))
+
 # Add all the lower case letters to the list
 for x in range(26):
-    dList[cLongBinary[x]] = chr(x+97)
+    dList[chr(x+97)] = cLongBinary[x]
 
 # Add all the upper case letter to the list
 for y in range(27, 53, 1):
-    dList[cLongBinary[y-1]] = chr(y+38)
-
-# Add the short bits 
-for i in range (32):
-	cShortBinary.append("1"+"{0:{fill}5b}".format(i,fill="0"))
+    dList[chr(y+38)] = cLongBinary[y-1]
 
 # First put all the long binary to adjust
-blist = ["0000000", "0000010", "0000011", "0000100", "0001000", "0001011","0001101","0001110", "0010001", "0010010", "0010011", "0010100"]
+blist = ["0000000","0000011","0000100","0000111","0001000","0001101","0001110","0010001","0010011"]
+
+# Add the rest of the long binarys from 53 to 64
 for z in range(52, 64):
     blist.append(cLongBinary[z])
 
+# Adds adds all the short binary to blist
 blist.extend(cShortBinary)
-# Then put all the values to be assigned to the long binary
-clist = ["at", "she", "as", "by", "on", "her", "but", "from", "not", "his", "say", "they", ".", ",", "then", "\'", "\"", "\n", "-", "!", "use", "do", "have", "are", "the", "be", "to", "of", " ", "in", "a", "or", "it", "for", "r", "this", "e", "he", "you", "d", "and", "that", "i", "n", "s", "with", "o", "u", "we", "t", "l", "c", "ate", "ear", "ed", "ion"]
+# Then put all the values to be assigned to the long binary blist
+clist = ["have","it","that","for","not","on","he","ea","as",".",",","this","\'","\"","\n","-","!","you","do","at","but"," ","e","t","a","o","i","n","h","r","d","the","to","be","ate","and","ion"]
 
 # Replace the definition of the existing binary
-for i in range (len(blist)):
-	dList[blist[i]]=clist[i]
+for i in range (len(clist)):
+	dList[clist[i]]=blist[i]
 
 # Create the short bits
 
@@ -57,7 +59,7 @@ for a, b in dList.items():
 print("")
 
 # Ask for file name
-file=input("What is your file name? (Example: type name for name.txt)")
+file=input("What is your file name? (Example: type name for name.txt)\n")
 # Open the text file (input source)
 userInput = open(file+".txt")
 
@@ -74,36 +76,29 @@ ask = input("Binary to String or String to binary?\n(1)Binary to String\n(2)Stri
 if ask == "1": 
 	# While the string is not fully read, it will keep going, if it detects 1 first, then it will read the next certain digits of binary, same for 0. Then the read piece will be used directly to find the right "key" and add to the outputTextList
 	while True:
-		if sIndex>len(myString)-6:
+		if sIndex>len(myString)-5:
 			break
 		elif myString[sIndex]=="1":
-			outputTextList.append(dList.get(myString[sIndex:sIndex+6]))
-			sIndex+=6
+			outputTextList.append(getkey(myString[sIndex:sIndex+5]))
+			sIndex+=5
 		elif myString[sIndex]=="0":
-			outputTextList.append(dList.get(myString[sIndex:sIndex+7]))
+			outputTextList.append(getkey(myString[sIndex:sIndex+7]))
 			sIndex+=7
 elif ask == "2":
 	# Set a list of number corresponding to the index of each letter
-	checkRemain=list(range(len(myString)))
-	print (checkRemain)
-	for length in range (5,0,-1):
-		for key, value in dList.items():
-			if len(value)==length:
-				for x in range(len(myString)):
-					if myString[x:x+length]==value and x in checkRemain and x+length-1 in checkRemain:
-						print(value)
-						wList[x]=getkey(value)
-						print(checkRemain)
-						for n in range(x,x+length):
-							print(n)
-							checkRemain.remove(n)
+	s=0
+	outputs=""
+	while (s<len(myString)):
+		for length in range(4,0,-1):
+			if myString[s:s+length] in dList:
+				outputs+=dList[myString[s:s+length]]
+				s+=length
 
-for a, b in sorted(wList.items()):
-    print(a,b)
-for a, b in sorted(wList.items()):
-    outputTextList.append(b)
 # Print the outputTextList
 print(outputTextList)
 
 # Joins all the pieces in the output list into one single string
-print(''.join(outputTextList))
+if ask=="1":
+	print(''.join(outputTextList))
+else:
+	print(len(outputs),'.',outputs)	
